fff3ec470125329b4901fa75b7ccc0a3a5b0606e
Kenji Nakamura <nakamura.kenji005@jp.panasonic.com>
2017-03-17 16:08:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/nkenji/clothapp/viewmodel/Cloth.java
@@ -0,0 +1,14 @@
+package com.example.nkenji.clothapp.viewmodel;
+
+public class Cloth {
+    public String name;
+    private Integer price;
+
+    public Cloth(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ app/src/main/java/com/example/nkenji/clothapp/viewmodel/MainViewModel.java
@@ -0,0 +1,11 @@
+package com.example.nkenji.clothapp.viewmodel;
+
+import android.util.Log;
+import android.view.View;
+
+public class MainViewModel {
+
+    public void onClickCloth(View view) {
+        Log.e("Tag","Error");
+    }
+}
